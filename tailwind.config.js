eff53fa3a88219840c14a6e4f57fa35fe4c3375a
Avaaz <avaaz@Avaazs-MacBook-Pro-2.local>
2022-12-24 11:58:36 +0330

    add all files

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      secondary: "#858fad",
+      primary: "#3fa9f5",
+      white: "#fff",
+      black: "#000",
+    },
+    extend: {
+      fontFamily: {
+        // fontName: "fontName",
+      },
+    },
+  },
+  corePlugins: {
+    preflight: false,
+  },
+
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant('xs-only', "@media screen and (max-width: theme('screens.md'))"); // instead of hard-coded 640px use sm breakpoint value from config. Or anything
+    }),
+  ],
+};
